96045f420a97a691af3c3510bdf88e664b303e1a
ShivamShrirao <shivamshrirao@gmail.com>
2019-09-07 06:44:34 +0530

    added gif

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Pygame 3d_cube simulation
+![](show.gif)
--- render.py
+++ render.py
@@ -1,10 +1,9 @@
 import pygame
 from pygame.locals import *
-from pygame import gfxdraw
 import numpy as np
 
-SCR_HEIGHT	= 800
-SCR_WIDTH	= 800
+SCR_HEIGHT	= 600
+SCR_WIDTH	= 600
 FPS			= 120		# I have 144 Hz monitor bish!
 COLOR		= (255,0,0)
 
@@ -13,36 +12,27 @@ class Point:
 		self.x = x
 		self.y = y
 		self.z = z
-		self.rx=(self.y**2+self.z**2)**0.5
-		self.ry=(self.z**2+self.x**2)**0.5
-		self.rz=(self.x**2+self.y**2)**0.5
-		self.alpha = np.arctan2(self.y,self.z)
-		self.beta = np.arctan2(self.z,self.x)
-		self.gamma = np.arctan2(self.x,self.y)
 
 	def xyz(self):
 		return self.x,self.y,self.z
 
-	def rotateX(self,angle=1):
+	def rotateX(self,angle=1):		#cos(a+tht), sin(a+tht)
 		theta=angle*np.pi/180
-		self.alpha+=theta
-		self.y=self.rz*np.cos(self.alpha)
-		self.z=self.rz*np.sin(self.alpha)
+		self.y=self.y*np.cos(theta) - self.z*np.sin(theta)
+		self.z=self.y*np.sin(theta) + self.z*np.cos(theta)
 
 	def rotateY(self,angle=1):
 		theta=angle*np.pi/180
-		self.beta+=theta
-		self.z=self.rz*np.cos(self.beta)
-		self.x=self.rz*np.sin(self.beta)
+		self.z=self.z*np.cos(theta) - self.x*np.sin(theta)
+		self.x=self.z*np.sin(theta) + self.x*np.cos(theta)
 
 	def rotateZ(self,angle=1):
 		theta=angle*np.pi/180
-		self.gamma+=theta
-		self.x=self.rz*np.cos(self.gamma)
-		self.y=self.rz*np.sin(self.gamma)
+		self.x=self.x*np.cos(theta) - self.y*np.sin(theta)
+		self.y=self.x*np.sin(theta) + self.y*np.cos(theta)
 
 	def project(self):
-		factor = 2000/(20+self.z)
+		factor = 2000/(15+self.z)
 		x = self.x*factor + SCR_WIDTH/2
 		y = self.y*factor + SCR_HEIGHT/2
 		return x,y
@@ -50,7 +40,7 @@ class Point:
 class Cube:
 	def __init__(self):
 		self.length = 200
-		self.points = [Point(-1,1,1),
+		self.points = [	Point(-1,1,1),
 						Point(1,1,1),
 						Point(1,-1,1),
 						Point(-1,-1,1),
@@ -61,11 +51,14 @@ class Cube:
 		self.ver1 = (0,2,5,7)
 		self.ver2 = ((1,3,4),(1,3,6),(1,4,6),(3,4,6))
 
-	def update(self,angle):
+	def update(self,x,y,z):
 		for p in self.points:
-			p.rotateX(angle)
-			# p.rotateY(angle)
-			# p.rotateZ(angle)
+			if x:
+				p.rotateX(x)
+			if y:
+				p.rotateY(y)
+			if z:
+				p.rotateZ(z)
 
 	def drawlines(self,screen):
 		pygame.font.init()
@@ -74,8 +67,6 @@ class Cube:
 		for i,b in zip(self.ver1,self.ver2):
 			for j in b:
 				pygame.draw.aaline(screen,COLOR,p[i].project(),p[j].project())
-				screen.blit(font.render(str(i),True,(0, 255, 0)),p[i].project())
-				screen.blit(font.render(str(i),True,(0, 255, 0)),p[j].project())
 
 class Render:
 	def __init__(self):
@@ -84,9 +75,8 @@ class Render:
 		self.cu1 = Cube()
 		self.angle = 1
 
-	def draw(self):
-		self.cu1.update(self.angle)
-		# self.angle=0
+	def draw(self,x,y,z):
+		self.cu1.update(x,y,z)
 		self.cu1.drawlines(self.screen)
 
 	def run(self):
@@ -97,8 +87,22 @@ class Render:
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					n_exit_game=False
-			self.screen.fill((255, 255, 255))
-			self.draw()
+			keys=pygame.key.get_pressed()
+			x=y=z=0
+			if keys[K_s]:
+				x=1
+			if keys[K_w]:
+				x=-1
+			if keys[K_a]:
+				y=1
+			if keys[K_d]:
+				y=-1
+			if keys[K_x]:
+				z=1
+			if keys[K_z]:
+				z=-1
+			self.screen.fill((0, 0, 0))
+			self.draw(x,y,z)
 			pygame.display.update()
 
 if __name__ == "__main__":
